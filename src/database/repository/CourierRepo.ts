92a81f4cc1e014e8a55bdd8ab131b983f31e1468
Alberto Soler Palau <albertosolpal@gmail.com>
2021-04-09 09:06:54 +0200

    Created delete courier endpoint

--- src/database/repository/CourierRepo.ts
+++ src/database/repository/CourierRepo.ts
@@ -1,3 +1,4 @@
+import { Types } from 'mongoose';
 import Courier, { CourierModel } from '../models/Courier';
 
 export default class CourierRepo {
@@ -10,5 +11,10 @@ export default class CourierRepo {
     return CourierModel.find({ max_capacity: { $gte: capacity_required } })
       .lean<[Courier]>()
       .exec();
+  }
+
+  public static async delete(id: Types.ObjectId): Promise<any> {
+    const deletedCourier = await CourierModel.deleteOne({ _id: id });
+    return deletedCourier;
   }
 }
--- src/router/courier/index.ts
+++ src/router/courier/index.ts
@@ -1,7 +1,8 @@
 import { Router, Request, Response } from 'express';
+import mongoose, { Types } from 'mongoose';
 import Logger from '../../core/Logger';
 
-import validator from '../../helpers/validator';
+import validator, { ValidationSource } from '../../helpers/validator';
 
 import CourierRepo from '../../database/repository/CourierRepo';
 import schema from './schema';
@@ -29,6 +30,18 @@ router.post('/courier', validator(schema.courierCreate), async (req: Request, re
     };
     const newCourier = await CourierRepo.create(courier);
     res.status(201).json(newCourier);
+  } catch (err) {
+    Logger.error(err);
+    res.status(500).send(err.message);
+  }
+});
+
+// DELETE
+router.delete('/courier/:id', validator(schema.courirerDelete, ValidationSource.PARAM), async (req: Request, res: Response) => {
+  try {
+    const _id: Types.ObjectId = mongoose.Types.ObjectId(req.params.id);
+    const deletedCourier = await CourierRepo.delete(_id);
+    res.status(200).json(deletedCourier);
   } catch (err) {
     Logger.error(err);
     res.status(500).send(err.message);
